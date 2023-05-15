8bfd45bb4412b9e9933c1f8d47b91656e78febfd
ismail <ismailbrahimi222000@gmail.com>
2023-05-15 17:42:20 +0200

    commit - adaptation d'une map 2D avec le front

--- assets/js/index.js
+++ assets/js/index.js
@@ -42,6 +42,8 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+
+
 // control that shows state info on hover
 var info = L.control();
 
