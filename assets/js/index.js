de531eb4a9d2939ac9684b8aab5f1152bbf0722f
ismail <ismailbrahimi222000@gmail.com>
2023-05-22 18:58:03 +0200

    Fin Sprint 1

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,6 @@
 import * as L from "leaflet";
-import * as geoData from "../../data/france_geojson.json";
+import * as geoData from "../../data/france_geojson_densite.json";
 
-// function getColor(d) {
-//     return d > 1000 ? '#800026' :
-//            d > 500  ? '#BD0026' :
-//            d > 200  ? '#E31A1C' :
-//            d > 100  ? '#FC4E2A' :
-//            d > 50   ? '#FD8D3C' :
-//            d > 20   ? '#FEB24C' :
-//            d > 10   ? '#FED976' :
-//                       '#FFEDA0';
-// }
-
-// var map = L.map('map').setView([51.505, -0.09], 13);
-
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
-
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
-
-// function style(feature) {
-//     return {
-//         fillColor: getColor(feature.properties.density),
-//         weight: 2,
-//         opacity: 1,
-//         color: 'white',
-//         dashArray: '3',
-//         fillOpacity: 0.7
-//     };
-// }
-
-// L.geoJson(geoData, {style: style}).addTo(map);
-// console.log(L.GeoJSON);
 
 var map = L.map("map").setView([51.505, -0.09], 4);
 
