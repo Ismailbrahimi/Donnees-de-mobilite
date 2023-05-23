80915b4b3c17df8c305f2f5fec4b6e7382634a1a
noobmaster122 <brahimi.a.farouk@gmail.com>
2023-05-23 11:15:39 +0200

    first implementation

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,165 +1,31 @@
 import * as L from "leaflet";
 import * as geoData from "../../data/france_geojson.json";
+import * as Routing from "leaflet-routing-machine";
+import {PathTracker} from "./services/PathTracker";
 
-// function getColor(d) {
-//     return d > 1000 ? '#800026' :
-//            d > 500  ? '#BD0026' :
-//            d > 200  ? '#E31A1C' :
-//            d > 100  ? '#FC4E2A' :
-//            d > 50   ? '#FD8D3C' :
-//            d > 20   ? '#FEB24C' :
-//            d > 10   ? '#FED976' :
-//                       '#FFEDA0';
-// }
+const pathTracker = new PathTracker(L);
+//gen map
+pathTracker.generateMap();
+// set tiles player
+pathTracker.setTilePlayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+});
+//set icon
+pathTracker.setIcon("../marker.png");
+// set routing control
+pathTracker.setRoutingControl(
+     [
+     pathTracker.L.latLng(48.573405, 7.752111), // Strasbourg
+    pathTracker.L.latLng(48.858222, 2.2945), // Paris
+     ]);
 
-// var map = L.map('map').setView([51.505, -0.09], 13);
+    pathTracker.setRoutingControl([
+    pathTracker.L.latLng(43.296482, 5.36978), // Marseille
+    pathTracker.L.latLng(45.767, 4.833), // Lyon
+    ]);
 
-// L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-// }).addTo(map);
+    pathTracker.setRoutingControl([
+    pathTracker.L.latLng(44.8378, -0.5792), // Bordeaux
+    pathTracker.L.latLng(48.1173, -1.6778), // Rennes
+     ]);
 
-// L.marker([51.5, -0.09]).addTo(map)
-//     .bindPopup('A pretty CSS popup.<br> Easily customizable.')
-//     .openPopup();
-
-// function style(feature) {
-//     return {
-//         fillColor: getColor(feature.properties.density),
-//         weight: 2,
-//         opacity: 1,
-//         color: 'white',
-//         dashArray: '3',
-//         fillOpacity: 0.7
-//     };
-// }
-
-// L.geoJson(geoData, {style: style}).addTo(map);
-// console.log(L.GeoJSON);
-
-var map = L.map("map").setView([51.505, -0.09], 4);
-
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-}).addTo(map);
-
-// control that shows state info on hover
-var info = L.control();
-
-info.onAdd = function(map) {
-  this._div = L.DomUtil.create("div", "info");
-  this.update();
-  return this._div;
-};
-
-info.update = function(props) {
-  this._div.innerHTML =
-    "<h4>US Population Density</h4>" +
-    (props
-      ? "<b>" +
-        props.name +
-        "</b><br />" +
-        props.density +
-        " people / mi<sup>2</sup>"
-      : "Hover over a state");
-};
-
-info.addTo(map);
-
-// get color depending on population density value
-function getColor(d) {
-  return d > 1000
-    ? "#800026"
-    : d > 500
-      ? "#BD0026"
-      : d > 200
-        ? "#E31A1C"
-        : d > 100
-          ? "#FC4E2A"
-          : d > 50
-            ? "#FD8D3C"
-            : d > 20 ? "#FEB24C" : d > 10 ? "#FED976" : "#FFEDA0";
-}
-
-function style(feature) {
-  return {
-    weight: 2,
-    opacity: 1,
-    color: "white",
-    dashArray: "3",
-    fillOpacity: 0.7,
-    fillColor: getColor(feature.properties.density)
-  };
-}
-
-function highlightFeature(e) {
-  var layer = e.target;
-
-  layer.setStyle({
-    weight: 5,
-    color: "#000",
-    dashArray: "",
-    fillOpacity: 0.7
-  });
-
-  if (!L.Browser.ie && !L.Browser.opera && !L.Browser.edge) {
-    layer.bringToFront();
-  }
-
-  info.update(layer.feature.properties);
-}
-
-var geojson;
-
-function resetHighlight(e) {
-  geojson.resetStyle(e.target);
-  info.update();
-}
-
-function zoomToFeature(e) {
-  map.fitBounds(e.target.getBounds());
-}
-
-function onEachFeature(feature, layer) {
-  layer.on({
-    mouseover: highlightFeature,
-    mouseout: resetHighlight,
-    click: zoomToFeature
-  });
-}
-
-geojson = L.geoJson(geoData, {
-  style: style,
-  onEachFeature: onEachFeature
-}).addTo(map);
-
-map.attributionControl.addAttribution(
-  'Population data &copy; <a href="http://census.gov/">US Census Bureau</a>'
-);
-
-var legend = L.control({ position: "bottomright" });
-
-legend.onAdd = function(map) {
-  var div = L.DomUtil.create("div", "info legend"),
-    grades = [0, 10, 20, 50, 100, 200, 500, 1000],
-    labels = [],
-    from,
-    to;
-
-  for (var i = 0; i < grades.length; i++) {
-    from = grades[i];
-    to = grades[i + 1];
-
-    labels.push(
-      '<i style="background:' +
-        getColor(from + 1) +
-        '"></i> ' +
-        from +
-        (to ? "&ndash;" + to : "+")
-    );
-  }
-
-  div.innerHTML = labels.join("<br>");
-  return div;
-};
-
-legend.addTo(map);
