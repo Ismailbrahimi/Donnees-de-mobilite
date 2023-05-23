974f460cc0f5bfdf6ac4bab76030e9a9ddaaf376
noobmaster122 <brahimi.a.farouk@gmail.com>
2023-05-23 13:29:51 +0200

    updating the map segmentation functionality

--- assets/js/index.js
+++ assets/js/index.js
@@ -33,7 +33,7 @@ var isTracking = false;
 ===================================================*/
 
 // inject leaflet into pathTracker
-//var map = L.map("map").setView([51.505, -0.09], 4);
+var map = L.map("map").setView([51.505, -0.09], 4);
 var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 });
